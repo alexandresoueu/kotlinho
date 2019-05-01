c684fa3da0412437ebaef9ec5b34371442f68879
Alexandre DEV <calexandredev@gmail.com>
2019-05-01 11:31:13 -0300

    Show person on Console

--- src/helloWorld.kt
+++ src/helloWorld.kt
@@ -1,3 +1,8 @@
 fun main() {
     println("Hello Kotlinho !!!")
+    val name: String = "Alexandre"
+    val age: Int = 26
+    val profession: String = "Software Developer"
+
+    println(name + " " + age + " "+ profession)
 }
