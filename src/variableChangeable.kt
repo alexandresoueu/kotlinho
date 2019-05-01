e724a297c557515a925bd6dbd24e9f754cd239f0
Alexandre DEV <calexandredev@gmail.com>
2019-05-01 19:17:20 -0300

    Examples variables Changeable and Cheangeless

---
+++ src/variableChangeable.kt
@@ -0,0 +1,16 @@
+fun main() {
+    // Variable Changeable
+    var name = "ET"
+    name = "ET Bilu"
+    println(name)
+
+    //Variable Changeless
+    val age = 10
+    //age = 11
+    println(age)
+
+    // val y -- necessary pass to type, when don't have initial value.
+    val x: Int
+    x = 15
+    println(x)
+}
