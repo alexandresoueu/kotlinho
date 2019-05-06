a7dac0dfe51547ac353f4abbd7e130c0779d4b94
AlexandreSouEu <calexandredev@gmail.com>
2019-05-06 14:22:00 -0300

    Example of PACKAGE

---
+++ src/base/main.kt
@@ -0,0 +1,13 @@
+package base
+
+fun welcome() {
+    println("Welcome to the Jungle!")
+}
+
+fun t1() {
+    println("t1 is a function")
+}
+
+fun t2() {
+    println("t2 is a function")
+}
---
+++ src/main.kt
@@ -0,0 +1,8 @@
+import base.*
+
+fun main() {
+    //Package
+    welcome()
+    t1()
+    t2()
+}
